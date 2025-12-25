7a99625e678498d2be654304020a1f87fba6e876
ankushn <ankush.negi@vecmocon.com>
2025-12-25 18:55:56 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: '/ankush-porfolio-next/',
-  basePath: '/ankush-porfolio-next',
-  output: 'export'
 };
 
 export default nextConfig;
